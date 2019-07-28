46fcf0a3604886633c1d57b37d88544f5b36743b
a64091937 <643717213@qq.com>
2019-07-28 15:42:41 +0800

    加入了Copyright内容

---
+++ main.c
@@ -0,0 +1,20 @@
+/*	
+ Copyright © 2019-2019, Zhang Hongda, a64091937. All Rights Reserved.
+ Description:A Standard STM32 Project
+ Author:Zhang Hongda
+ Creation time:2019-7-28
+ */
+#include "stm32f4xx.h" 
+
+int main(void)
+{	
+	/*开启 GPIOH 时钟，使用外设时都要先开启它的时钟*/
+	
+	while(1);
+
+}
+
+// 函数为空，目的是为了骗过编译器不报错
+void SystemInit(void)
+{	
+}
