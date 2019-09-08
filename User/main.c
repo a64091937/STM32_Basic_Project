f17d4ba6bf9ac9931ee8fc3be74145039371800e
a64091937 <643717213@qq.com>
2019-09-08 23:15:17 +0800

    加入了led的代码

--- User/main.c
+++ User/main.c
@@ -16,19 +16,63 @@
   */
   
 #include "stm32f4xx.h"
+#include "./led/bsp_led.h" 
+
+void Delay(__IO u32 nCount); 
 
 /**
   * @brief  主函数
   * @param  无
   * @retval 无
   */
-
 int main(void)
 {
     for (;;) {
+        LED1( ON ); 		 // 亮 
+        Delay(0xFFFFFF);
+    	LED1( OFF );		  // 灭
+    	
+    	LED2( ON ); 		// 亮 
+    	Delay(0xFFFFFF);
+    	LED2( OFF );		  // 灭
+    	
+    	LED3( ON ); 		 // 亮 
+    	Delay(0xFFFFFF);
+    	LED3( OFF );		  // 灭	
+    	
+    	/*轮流显示 红绿蓝黄紫青白 颜色*/
+    	LED_RED;
+    	Delay(0xFFFFFF);
+    	
+    	LED_GREEN;
+    	Delay(0xFFFFFF);
+    	
+    	LED_BLUE;
+    	Delay(0xFFFFFF);
+    	
+    	LED_YELLOW;
+    	Delay(0xFFFFFF);
+    	
+    	LED_PURPLE;
+    	Delay(0xFFFFFF);
+    			
+    	LED_CYAN;
+    	Delay(0xFFFFFF);
+    	
+    	LED_WHITE;
+    	Delay(0xFFFFFF);
+    	
+    	LED_RGBOFF;
+    	Delay(0xFFFFFF);
 
         
     }    
 
 }
+
+void Delay(__IO uint32_t nCount)	 //简单的延时函数
+{
+	for(; nCount != 0; nCount--);
+}
+
 
