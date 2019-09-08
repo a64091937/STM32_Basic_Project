9386e0457ab72ba7dc6642040e5ad9cc32451bf3
a64091937 <643717213@qq.com>
2019-09-08 20:17:05 +0800

    修改一下文件头.支持Doxygen注释规范

--- User/main.c
+++ User/main.c
@@ -1,9 +1,19 @@
-/*	
- Copyright ? 2019-2019, Zhang Hongda, a64091937. All Rights Reserved.
- Description:A Standard Build C Project
- Author:Zhang Hongda
- Creation time:2019-7-28
- */
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Zhang Hongda, a64091937
+  * @version V1.0
+  * @date    2019-9-8
+  * @brief   A Standard STM32 Project
+  ******************************************************************************
+  * @attention
+  * 程序来到main函数之前，启动文件：statup_stm32f429xx.s已经调用
+  * SystemInit()函数把系统时钟初始化成180MHZ
+  *  SystemInit()在system_stm32f4xx.c中定义
+  * 如果用户想修改系统时钟，可自行编写程序修改
+  *
+  ******************************************************************************
+  */
   
 #include "stm32f4xx.h"
 
@@ -12,17 +22,13 @@
   * @param  无
   * @retval 无
   */
+
 int main(void)
 {
-		/* 程序来到main函数之前，启动文件：statup_stm32f429xx.s已经调用
-		* SystemInit()函数把系统时钟初始化成180MHZ
-		* SystemInit()在system_stm32f4xx.c中定义
-		* 如果用户想修改系统时钟，可自行编写程序修改
-		*/
-		  /* add your code here ^_^. */
-  	  while(1);
+    for (;;) {
+
+        
+    }    
 
 }
-
-/*********************************************END OF FILE**********************/
 
